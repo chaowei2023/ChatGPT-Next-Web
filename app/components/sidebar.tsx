d607c408105600fff9aa6ee0e63eaad654de1c4e
chaowei2023 <133602554+chaowei2023@users.noreply.github.com>
2023-11-07 07:03:54 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import styles from "./home.module.scss";
 
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/black-bot.svg";
+import GithubIcon from "../icons/chat.svg";
 import ChatGptIcon from "../icons/chatgpt.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
