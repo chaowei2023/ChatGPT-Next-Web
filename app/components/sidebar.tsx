724863f291825a8d31dbfcd21e30048804582a93
chaowei2023 <133602554+chaowei2023@users.noreply.github.com>
2023-11-07 06:30:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -207,7 +207,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={ai.fxmao.cn} target="_blank" rel="noopener noreferrer">
+            <a href="https://ai.fxmao.cn" target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
